8684bda366a31c4820cdfe0665fdadb7c2842b40
Miguel Alvarez <kamapu78@gmail.com>
2021-01-17 12:20:03 +0100

    Test issues solved

--- tests/testthat/test-file_list.R
+++ tests/testthat/test-file_list.R
@@ -5,7 +5,7 @@ test_that("files can be handled in separated data frames", {
 			
 			Files <- file_list(x = openaccess)
 			openaccess <- openaccess[ , colnames(openaccess) != "file"]
-			expect_equal(file_list(x = openaccess), NULL)
+			expect_equal(openaccess$file, NULL)
 			
 			expect_is({file_list(openaccess) <- Files
 					openaccess}, "lib_df")
