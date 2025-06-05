bc112f6254990f3725956fcf1b0b87a6fd1afeb9
Miguel Alvarez <kamapu@posteo.de>
2025-06-05 12:11:03 +0200

    bib2database adapted to new sql strategy.

--- R/bib2database.R
+++ R/bib2database.R
@@ -50,12 +50,12 @@ setMethod(
     }
     query <- character(0)
     # Create tables
-    query <- c(query, paste(
-      "create table", paste0(schema, ".main_table"),
+    query <- c(query, paste0(
+      "create table \"", paste0(schema, "\".main_table"),
       "()"
     ))
-    query <- c(query, paste(
-      "create table", paste0(schema, ".file_list"),
+    query <- c(query, paste0(
+      "create table \"", paste0(schema, "\".file_list"),
       "()"
     ))
     # Add columns to main table
@@ -63,21 +63,23 @@ setMethod(
     names(def_main_table) <- c("name", "comment")
     def_main_table$type <- "text"
     def_main_table$type[def_main_table$name == "bibtexkey"] <-
-        "text primary key"
-    query <- c(query, add_columns(conn,
-      df = def_main_table,
-      name = c(schema, "main_table"), eval = FALSE
-    ))
+      "text primary key"
+    query <- c(query, add_columns_sql(def_main_table, c(schema, "main_table")))
+
     # Add columns to file list
     def_file_list <- bib_tags$file_list[, c("field", "description")]
     names(def_file_list) <- c("name", "comment")
     def_file_list$type <- "text"
-    def_file_list <- do.call(rbind, list(def_file_list,
-          data.frame(name = "file_id", comment = "Identifier for file entry.",
-              type = "serial primary key")))
-    query <- c(query, add_columns(conn,
+    def_file_list <- do.call(rbind, list(
+      def_file_list,
+      data.frame(
+        name = "file_id", comment = "Identifier for file entry.",
+        type = "serial primary key"
+      )
+    ))
+    query <- c(query, add_columns_sql(
       df = def_file_list,
-      name = c(schema, "file_list"), eval = FALSE
+      name = c(schema, "file_list")
     ))
     # Foreign key
     query <- c(query, paste0(
@@ -85,7 +87,7 @@ setMethod(
       "add constraint fk_bibtexkey foreign key (bibtexkey) ",
       "references ", schema, ".main_table (bibtexkey)"
     ))
-    class(query) <- c("sql", "character")
+    query <- as(query, "sql")
     if (eval) {
       dbSendQuery(conn, query)
       message("DONE!")
@@ -93,10 +95,6 @@ setMethod(
     invisible(query)
   }
 )
-
-
-
-
 
 #' @rdname bib2database
 #' @aliases bib2database,PostgreSQLConnection,character,lib_db-method
@@ -106,19 +104,33 @@ setMethod(
     schema = "character", bib = "lib_db"
   ),
   function(conn, schema, bib, eval = TRUE, ...) {
+    # Write empty table
     query <- bib2database(conn = conn, schema = schema, eval = FALSE)
-    for (i in names(bib@main_table)) {
-      bib@main_table[[i]] <- gsub("'", "''", bib@main_table[[i]],
-        fixed = TRUE
-      )
+    # Insert rows
+    missing_columns <- names(bib@main_table)[!names(bib@main_table) %in%
+      bib_tags$tags_bib$field]
+    if (length(missing_columns)) {
+      warning(paste0(
+        "Following columns are not in database ",
+        "and will be ignored:\n", paste0(missing_columns, collapse = ", ")
+      ))
     }
-    query <- c(query, insert_rows(conn, bib@main_table, c(schema, "main_table"),
-      eval = FALSE
-    ))
-    query <- c(query, insert_rows(conn, bib@file_list, c(schema, "file_list"),
-      eval = FALSE
-    ))
-    class(query) <- c("sql", "character")
+    # Insert rows in main table
+    if (nrow(bib@main_table)) {
+      query <- c(query, insert_rows_sql(
+        bib@main_table,
+        c(schema, "main_table")
+      ))
+      if (!is.null(bib@file_list)) {
+        if (nrow(bib@file_list)) {
+          query <- c(query, insert_rows_sql(
+            bib@file_list,
+            c(schema, "file_list")
+          ))
+        }
+      }
+    }
+    query <- as(query, "sql")
     if (eval) {
       dbSendQuery(conn, query)
       message("DONE!")
@@ -126,10 +138,6 @@ setMethod(
     invisible(query)
   }
 )
-
-
-
-
 
 #' @rdname bib2database
 #' @aliases bib2database,PostgreSQLConnection,character,lib_df-method
@@ -140,12 +148,7 @@ setMethod(
   ),
   function(conn, schema, bib, eval = TRUE, ...) {
     bib <- as(bib, "lib_db")
-    query <- bib2database(conn = conn, schema = schema, bib = bib, eval = FALSE)
-    if (eval) {
-      dbSendQuery(conn, query)
-      message("DONE!")
-    }
-    invisible(query)
+    bib2database(conn, schema, bib, eval, ...)
   }
 )
 
@@ -163,14 +166,6 @@ setMethod(
     if (length(conn@dir$schema) == 0) {
       stop("Name of schema is missing in input object.")
     }
-    query <- bib2database(
-      conn = conn@dir$connection, schema = conn@dir$schema,
-      bib = conn, eval = FALSE
-    )
-    if (eval) {
-      dbSendQuery(conn, query)
-      message("DONE!")
-    }
-    invisible(query)
+    bib2database(conn@dir$connection, conn@dir$schema, conn, eval, ...)
   }
 )
--- lab/ex-bib2database.R
+++ lab/ex-bib2database.R
@@ -8,7 +8,8 @@ cred <- credentials()
 conn <- connect_db("test_bibrefs", user = cred["user"],
     password = cred["password"])
 
-# create empty database
+# create empty database --------------------------------------------------------
+dbSendQuery(conn, "drop schema bibrefs_empty cascade")
 dbSendQuery(conn, "create schema bibrefs_empty")
 
 ## only query
@@ -18,9 +19,36 @@ query
 ## run query
 bib2database(conn, "bibrefs_empty", eval = TRUE)
 
-# create database with rows on it (further methods)
+# create database with rows on it ----------------------------------------------
+dbSendQuery(conn, "drop schema bibrefs_content cascade")
+dbSendQuery(conn, "create schema bibrefs_content")
 
-# go to update database
+# Load the data
+bibrefs <- read_bib(file.path(path.package("biblio"), "LuebertPliscoff.bib"))
 
+#bib1 <- as(bibrefs[1:10], "lib_db")
 
+## only query
+query <- bib2database(conn, "bibrefs_content", bibrefs[1:10, ], eval = FALSE)
+query
+
+## Fill the database
+bib2database(conn, "bibrefs_content", bibrefs)
+
+# Including all information in the object --------------------------------------
+bibrefs <- as(openaccess, "lib_db")
+bibrefs@dir$connection <- conn
+bibrefs@dir$folder <- "inst/articles" # Change to package folder
+bibrefs@dir$schema <- "bibrefs_full"
+
+dbSendQuery(conn, "drop schema bibrefs_full cascade")
+dbSendQuery(conn, "create schema bibrefs_full")
+
+# Only query
+query <- bib2database(bibrefs, eval = FALSE)
+query
+
+bib2database(bibrefs)
+
+# Close connection
 disconnect_db(conn)
